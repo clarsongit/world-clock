3f2adb820e7fc97eea19055197298505c300361e
clarsongit <clarson@berkeley.edu>
2023-01-16 13:36:55 -0800

    Added Lisbon to home page

--- javascript/index.js
+++ javascript/index.js
@@ -25,6 +25,19 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     )}`;
   }
+
+  // Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
+
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = `${lisbonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
